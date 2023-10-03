de4c9a802ffce80fa25fe291dbbb8259b76feb2c
Lapin Dmytro <lapin_dm@yahoo.com>
2023-10-03 17:22:52 +0200

    made some start works to push in *Best-selling dishes* exact dishes.

--- src/modules/Charts/PieChart.jsx
+++ src/modules/Charts/PieChart.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { ResponsiveContainer, PieChart, Pie } from 'recharts';
+import { database} from "../../firebase_config";
+import { ref } from "firebase/database";
 
 const data = [
   { name: 'Dish 1', value: 132 },
@@ -10,7 +12,13 @@ const data = [
   { name: 'Dish 6', value: 64 },
 ];
 
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+const formattedDate = `${year}-${month}`;
+
 const ChartPie = () => {
+  const dishesRef = ref(database, formattedDate);
   return (
     <div style={{ width: '75%', height: 300 }}>
       <ResponsiveContainer>
