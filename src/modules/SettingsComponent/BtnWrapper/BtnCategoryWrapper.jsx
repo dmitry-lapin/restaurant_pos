19ad8c155e04445ac6a1ee488532d15cb8480843
Lapin Dmytro <lapin_dm@yahoo.com>
2023-11-19 01:05:26 +0100

    i decide to change a layout of my settings page. Second half, so i finish my settings button layout, next gonna be some fixes inside the tab.

--- src/modules/SettingsComponent/BtnWrapper/BtnCategoryWrapper.jsx
+++ src/modules/SettingsComponent/BtnWrapper/BtnCategoryWrapper.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
-const BtnCategoryWrapper = ({ category, onClick }) => {
+const BtnCategoryWrapper = ({ category, icon, description, onClick }) => {
     return (
-      <button className="border bg-zinc-50 rounded" onClick={() => onClick(category)}>
-        {category}
+      <button className="hover:bg-gray-300 hover:border-gray-400 hover:shadow-md border border-gray-300 rounded-lg px-4 py-3 duration-100" onClick={() => onClick(category)}>
+        <div className="flex flex-row space-x-3">
+            <div className="flex items-center justify-center">{icon}</div>
+            <div className="flex flex-col text-start">
+                <p className="font-semibold capitalize text-md">{category}</p>
+                <p className="text-xs text-zinc-500 font-normal">{description}</p>
+            </div>
+        </div>
       </button>
     );
   };
--- src/modules/SettingsComponent/SettingsComponent.jsx
+++ src/modules/SettingsComponent/SettingsComponent.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import ModuleHeaderName from "../../UI/ModuleHeaderName";
 import TextHeader from "../../UI/textHeader";
-import { userInformation } from "../../common_data";
 import General from "./TabsContent/GeneralTab";
 import Profile from "./TabsContent/ProfileTab";
 import Security from "./TabsContent/SecurityTab";
@@ -20,7 +19,40 @@ const SettingsComponent = () => {
     setActiveCategory(null);
   };
 
-  const categories = ['profile', 'notifications', 'email'];
+  const categories = [
+    {
+        category: 'general',
+        icon: 
+          (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.2} stroke="currentColor" className="w-9 h-9">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+          </svg>),
+        description: 'View your general details',
+    },
+    {
+      category: 'profile',
+      icon: 
+        (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.2} stroke="currentColor" className="w-9 h-9">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+        </svg>),
+      description: 'View your profile',
+    },
+    {
+      category: 'email',
+      icon: 
+        (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.2} stroke="currentColor" className="w-9 h-9">
+            <path strokeLinecap="round" d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25" />
+        </svg>),
+      description: 'View your notifications settings',
+    },
+    {
+        category: 'security',
+        icon: 
+            (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.2} stroke="currentColor" className="w-9 h-9">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+            </svg>),
+        description: 'View your security rules',
+    },
+  ];
 
     return(
         <div className="flex flex-1 flex-col bg-gray-200">
@@ -31,9 +63,15 @@ const SettingsComponent = () => {
             <div className="self-center flex-1 py-10 w-10/12">
                 {!activeCategory && (
 
-                <div className="space-x-6">
-                    {categories.map((category) => (
-                        <BtnCategoryWrapper key={category} category={category} onClick={openCategory} />
+                <div className="grid grid-cols-3 gap-5">
+                    {categories.map(({ category, icon, description }) => (
+                        <BtnCategoryWrapper
+                            key={category}
+                            category={category}
+                            icon={icon}
+                            description={description}
+                            onClick={openCategory}
+                        />
                     ))}
                 </div>
                 )}
@@ -43,7 +81,7 @@ const SettingsComponent = () => {
             { activeCategory === 'email' && <Email /> }
 
             { activeCategory && <button onClick={goBack}>Back</button> }
-      </div>
+            </div>
         </div>
     );
 }
