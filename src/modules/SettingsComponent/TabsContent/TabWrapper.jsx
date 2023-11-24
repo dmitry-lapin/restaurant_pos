1e5e149e0aabdfe47fc96c80b769704915aec250
Lapin Dmytro <lapin_dm@yahoo.com>
2023-11-24 13:57:33 +0100

    i made some features in security section of settings.

--- src/modules/SettingsComponent/TabsContent/TabWrapper.jsx
+++ src/modules/SettingsComponent/TabsContent/TabWrapper.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 
-const TabWrapper = ({children, name, description}) => {
+const  TabWrapper = ({children}) => {
     return(
         <div className="flex flex-col">
-            <div className="">
-                <p className="text-zinc-600">{description}</p>
-            </div>
                 {children}
         </div>
     );
