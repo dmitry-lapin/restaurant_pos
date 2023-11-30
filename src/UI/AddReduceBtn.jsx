e9a46149ead5c69911c3e179ac65c8767f75c60f
Lapin Dmytro <lapin_dm@yahoo.com>
2023-11-30 23:50:04 +0100

    done with catalog both color schemes.

--- src/UI/AddReduceBtn.jsx
+++ src/UI/AddReduceBtn.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const AddReduceBtn = ({action, bgColor="bg-white", color="text-black", hoverBg="hover:bg-zinc-200", onClick}) => {
+const AddReduceBtn = ({action, bgColor="bg-white", color="text-black", hoverBg="hover:bg-zinc-300 dark:hover:bg-zinc-600", onClick}) => {
     let styleBtn = "flex items-center justify-center rounded-md w-8 h-8 text-2xl font-semibold shadow-sm duration-100 " + hoverBg + " " + bgColor + " " + color;
 
     return(
--- src/modules/Main_catalog/components/dish_component/Product_card.jsx
+++ src/modules/Main_catalog/components/dish_component/Product_card.jsx
@@ -18,16 +18,16 @@ const ProductCard = ({ dish }) => {
   }
 
   return (
-    <div className="flex flex-col border bg-zinc-100 rounded-2xl shadow-md hover:shadow-xl duration-100 w-44 md:w-52">
+    <div className="flex flex-col border dark:border-zinc-800 bg-zinc-100 dark:bg-zinc-800 rounded-2xl shadow-md hover:shadow-xl duration-100 w-44 md:w-52">
       <div>
         <img src={dish.image} alt="Product" className="w-full h-32 md:h-40 rounded-t-2xl" />
       </div>
-      <div className="flex flex-col justify-between px-2 mb-2 mt-2 md:mt-0 md:px-3 md:py-4 h-full">
+      <div className="flex flex-col justify-between px-2 mb-2 mt-2 md:mt-0 md:px-3 md:py-4 h-full text-zinc-950 dark:text-gray-300">
         <p className="text-sm md:text-md font-semibold mb-2">{dish.name}</p>
-        <p className="text-gray-500 text-xs md:text-sm">{dish.description}</p>
+        <p className="text-gray-500 dark:text-zinc-400 text-xs md:text-sm">{dish.description}</p>
         <div className="flex items-center justify-between mt-3">
           <p className="text-md font-semibold">$ {dish.price}</p>
-          <AddReduceBtn action="+" bgColor="bg-white" onClick={handleAddToCart}/>
+          <AddReduceBtn action="+" bgColor="bg-zinc-200 dark:bg-zinc-700" onClick={handleAddToCart}/>
         </div>
       </div>
       <Toaster />
