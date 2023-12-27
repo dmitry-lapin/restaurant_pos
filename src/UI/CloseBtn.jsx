981ce05d67f47328337829eece68cdedf18b0508
Lapin Dmytro <lapin_dm@yahoo.com>
2023-12-27 23:33:32 +0100

    i decide to change my main dishes section, because it was pretty raw and not comfy. that's my first draws and concept of future section.

--- src/UI/CloseBtn.jsx
+++ src/UI/CloseBtn.jsx
@@ -3,7 +3,7 @@ import CloseButton from '../assets/common_icons/close.png';
 
 const CloseBtn = ({onClick}) => {
     return(
-        <button onClick={onClick} className=" text-md duration-100 px-2 md:px-4 py-1 md:py-2 rounded-full hover:shadow-lg bg-zinc-400 hover:bg-zinc-500 dark:bg-zinc-800 dark:hover:bg-zinc-700">
+        <button onClick={onClick} className="">
             <img src={CloseButton} alt="close_btn" className="w-7 h-7 md:w-10 md:h-10"/>
         </button>
     );
--- src/modules/Main_catalog/components/categories_feed/SelectedCategory.jsx
+++ src/modules/Main_catalog/components/categories_feed/SelectedCategory.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, createContext } from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "../dish_component/Product_card";
 import FeedWrapper from "../feed_wrapper/FeedWrapper";
 import TextHeader from "../../../../UI/textHeader";
@@ -8,7 +8,6 @@ import { database } from "../../../../firebase_config";
 import { ref, onValue, off } from "firebase/database";
 
 const SelectedCategory = () => {
-  
 
   const [dishes, setDishes] = useState({});
   const selectedCategory = useSelector((state) => state.Categories.selectedCategory);
@@ -27,31 +26,18 @@ const SelectedCategory = () => {
   }, []);
 
   const filteredDishes = selectedCategory === "All" ? Object.keys(dishes) : [selectedCategory];
+  console.log(filteredDishes)
+  console.log(dishes)
 
   return (
     <>
-      <TextHeader text="Selected Category" size="text-md md:text-lg p-4" color="text-zinc-950 dark:text-gray-300"/>
-      <div className="mb-4 p-3">
-        <FeedWrapper foodCategory={true}>
-          {filteredDishes.map((category, index) => (
-            <React.Fragment key={index}>
-              <TextHeader
-                text={category}
-                categoryTopic={true}
-                size="ml-2 text-md md:text-lg border-l-4 border-l-blue-500"
-                color="text-zinc-950 dark:text-gray-300"
-              />
-              <CategoryFeed
-                toWork={dishes[category] ? (
-                  dishes[category].map((data, key) => (
-                    <ProductCard key={key} dish={data} />
-                  ))
-                ) : null}
-                scroll={false}
-              />
-            </React.Fragment>
+      <TextHeader text="Explore our best menu" size="text-md md:text-xl p-4" color="text-zinc-950 dark:text-gray-300"/>
+      <div className="grid grid-cols-4 gap-4">
+      {filteredDishes.map((category, index) => (
+              dishes[category].map((value) => (
+                <ProductCard key={index} dish={value}/>
+              ))
           ))}
-        </FeedWrapper>
       </div>
     </>
   );
