fea5b6b5ea17e2dccc1c8941754a668b07eac2aa
Lapin Dmytro <lapin_dm@yahoo.com>
2023-12-28 00:24:50 +0100

    add few more changes in Product_card aswell. with data positioning, and so on.

--- src/modules/Main_catalog/components/dish_component/Product_card.jsx
+++ src/modules/Main_catalog/components/dish_component/Product_card.jsx
@@ -23,9 +23,11 @@ const ProductCard = ({ dish }) => {
         <span className="absolute bg-blur backdrop-filter bg-white/50 backdrop-blur-sm rounded-full px-3.5 py-1.5 bottom-4 left-4 text-black font-medium">Available</span>
         <img src={dish.image} alt="Product" className="w-full h-full" />
       </div>
-      <div className="flex flex-col text-zinc-950 dark:text-gray-300 px-1 space-y-1">
-        <p className="text-sm md:text-base font-semibold">{dish.name}</p>
-        <p className="text-gray-500 dark:text-zinc-400 text-xs md:text-sm">{dish.description}</p>
+      <div className="flex flex-col flex-1 justify-between text-zinc-950 dark:text-gray-300 px-1 space-y-1">
+        <div className="space-y-1">
+          <p className="text-sm md:text-base font-semibold">{dish.name}</p>
+          <p className="text-gray-500 dark:text-zinc-400 text-xs md:text-sm">{dish.description}</p>
+        </div>
         <div className="flex items-center justify-between">
           <p className="text-lg font-semibold">$ {dish.price}</p>
           <AddReduceBtn action="+" bgColor="bg-zinc-200 dark:bg-zinc-700" onClick={handleAddToCart}/>
