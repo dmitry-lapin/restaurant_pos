bd00986b22be66d0689c6e4f8a8fca3961826594
Lapin Dmytro <lapin_dm@yahoo.com>
2024-01-06 23:29:05 +0100

    made big change in category_items section. add arrows to make navigation between categories easier.

--- src/modules/Main_catalog/components/categories_feed/categories.jsx
+++ src/modules/Main_catalog/components/categories_feed/categories.jsx
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import TextHeader from "../../../../UI/textHeader";
-import CategoryFeed from "./Category_feed";
-import FeedWrapper from "../feed_wrapper/FeedWrapper";
+
 import CategoryBtn from "../../../../UI/CategoryBtn";
 
 import { ref, onValue, off } from "firebase/database";
 import { database } from "../../../../firebase_config";
 
 const Categories = () => {
+    const scrollContainer = useRef(null);
+
     const [categories, setCategories] = useState([]);
     useEffect(() => {
         const categoriesRef = ref(database, "categories");
@@ -26,18 +27,42 @@ const Categories = () => {
         return () => {
           off(categoriesRef, "value");
         };
+
+        
         
       }, []);
 
+      const scroll = (direction) => {
+        const scrollAmount = 100; // Можно настроить
+        if(scrollContainer.current) {
+            const scrollLeft = direction === 'left' ? -scrollAmount : scrollAmount;
+            scrollContainer.current.scrollBy({ left: scrollLeft, behavior: 'smooth' });
+        }
+    };
     return(
-        <FeedWrapper>
+        <>
             <TextHeader text="Categories" color="text-zinc-950 dark:text-gray-300" categoryTopic={true} size="text-md md:text-lg pt-4"/>
-            <CategoryFeed toWork={
-                categories.map((category, index) => (
-                    <CategoryBtn key={index} name={category.name} image={category.image} />
-                ))
-            }/>
-        </FeedWrapper>
+            <div className="flex items-center py-1.5 md:py-3 mx-2 md:px-5 bg-zinc-300 dark:bg-zinc-900 rounded-lg">
+            <button onClick={() => scroll('left')} className="text-white">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+              </svg>
+            </button>
+            <div ref={scrollContainer} className="flex flex-grow overflow-x-scroll rounded-md mx-3 py-2">
+                    <div className="flex flex-shrink-0 space-x-3 my-2">
+                        {categories.map((category, index) => (
+                            <CategoryBtn key={index} name={category.name} image={category.image} />
+                        ))}
+                    </div>
+                </div>
+                <button onClick={() => scroll('right')} className="text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                </svg>
+
+            </button>
+            </div>
+        </>
     );
 }
 
